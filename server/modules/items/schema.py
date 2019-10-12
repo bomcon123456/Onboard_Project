45515473b69bebe39d0e5ed7a21fb7a0d07a6395
Termanteus <bomcon123456@gmail.com>
2019-10-12 23:49:27 +0700

    Fix some more Schema v2

--- server/modules/items/schema.py
+++ server/modules/items/schema.py
@@ -8,8 +8,8 @@ class ItemSchema(Schema):
     title = fields.String(validate=validate.Length(min=4, max=30), required=True)
     description = fields.String(validate=validate.Length(min=4, max=256), required=True)
     category_id = fields.Integer(required=True)
-    user_id = fields.Integer(required=True)
-    seller = fields.Nested(UserSchema(only=['name']))
+    # user_id = fields.Integer(required=True)
+    seller = fields.Nested(UserSchema(only=['name', 'id']))
 
     class Meta:
         strict = True
