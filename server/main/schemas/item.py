dc94144a252f9c6b1da2449f26d42d3cef854daf
Troy <bomcon123456@gmail.com>
2019-10-17 13:34:01 +0700

    Update ItemSchema

--- server/main/schemas/item.py
+++ server/main/schemas/item.py
@@ -1,11 +1,14 @@
 from marshmallow import Schema, fields, validate
 
+from main.schemas.category import CategorySchema
+
 
 class ItemSchema(Schema):
     id = fields.Integer(required=True, dump_only=True)
     title = fields.String(validate=validate.Length(min=4, max=30), required=True)
     description = fields.String(validate=validate.Length(min=4, max=256), required=True)
-    category_id = fields.Integer(required=True)
+    category_id = fields.Integer(required=True, load_only=True)
+    category = fields.Nested(CategorySchema(only=('id', 'title')))
     creator_id = fields.Integer(required=True)
     created = fields.DateTime(dump_only=True)
     updated = fields.DateTime(dump_only=True)
