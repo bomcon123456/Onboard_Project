12a64f659d518629928a46b68c85af3c2dfefdc8
Troy <bomcon123456@gmail.com>
2019-10-22 17:26:30 +0700

    Update 2210

--- server/main/utils/type_conversions.py
+++ server/main/utils/type_conversions.py
@@ -1,4 +1,11 @@
 def pagination_params_conversion(page, per_page):
+    """
+    This function verify and converse the page/ per_page query params when a request is coming
+
+    :param page: the current page that the client is requesting
+    :param per_page: number of items per page
+    :return: None if these two params is not proper, a tuple (page,per_page) of type int if is good
+    """
     try:
         page = int(page)
         per_page = int(per_page)
