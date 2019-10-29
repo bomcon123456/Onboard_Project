96d34f693683e45c54d4ec2a002f5d956a1f9e8a
Troy <bomcon123456@gmail.com>
2019-10-29 09:39:28 +0700

    Fix Str->String marshmallow

--- server/main/schemas/user.py
+++ server/main/schemas/user.py
@@ -3,11 +3,11 @@ from marshmallow import Schema, fields, validate
 
 class UserRegisterSchema(Schema):
     email = fields.Email(required=True)
-    password = fields.Str(validate=validate.Length(min=4), required=True)
+    password = fields.String(validate=validate.Length(min=4), required=True)
 
 
 class UserSchema(Schema):
-    id = fields.Str(required=True, dump_only=True)
+    id = fields.String(required=True, dump_only=True)
     email = fields.Email(required=True)
     created = fields.DateTime(dump_only=True)
     updated = fields.DateTime(dump_only=True)
